58ea6d87c4eb7be2e9ad77c3aac6b43cdefb170f
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2023-08-15 13:55:37 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App (){
   function getIDB(){
     axios({
       method: "GET",
-      url:"/get_ib_front",
+      url:"http://khv2022.pythonanywhere.com/constructor/get_ib_front",
     })
     .then((response) => {
       setRequestStatus(1);
